733a2f8e872f4d867591f60d942e0c84cf982956
NextPr0gram <anafk2712@gmail.com>
2023-03-19 22:49:45 +0000

    Added methods to Window

--- src/main/engine/Window.java
+++ src/main/engine/Window.java
@@ -1,10 +1,15 @@
 package main.engine;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Window {
     private int width, height;
     private String title;
+    private long glfwWindow;
 
     private static Window window = null;
 
@@ -23,5 +28,33 @@ public class Window {
 
     public void run() {
         System.out.println("LWJGL " + Version.getVersion());
+
+        init();
+        loop();
     }
+
+    private void init() {
+        // Setup rror callback: this is where GLFW will print to if there are any errors
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(width, height, title, NULL, NULL);
+        
+
+    }
+
+    private void loop() {
+    }
+
 }
